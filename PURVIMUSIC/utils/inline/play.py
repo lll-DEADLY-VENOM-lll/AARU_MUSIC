8833cd3428a7f26dd822934f9a5bc8b1a35cb0a0
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-01-05 10:12:53 +0530

    0.3 version

--- PURVIMUSIC/utils/inline/play.py
+++ PURVIMUSIC/utils/inline/play.py
@@ -1,5 +1,7 @@
 import math
 from pyrogram.types import InlineKeyboardButton
+from PURVIMUSIC import app
+import config
 from PURVIMUSIC.utils.formatters import time_to_seconds
 
 def track_markup(_, videoid, user_id, channel, fplay):
